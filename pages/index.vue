ebbe98e98b07e091016346a94443fb993a5b94cf
Hirokazu Degawa <hxdegawa@gmail.com>
2021-01-28 00:16:15 +0900

    feat: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .top
+    p Hello, World!
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({})
+export default class IndexPage extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.top {
+  @include page-base;
+}
+</style>
